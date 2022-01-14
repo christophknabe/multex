557309b62e8bf6d8b41d24d44143e38d166bf8ac
Christoph Knabe <knabe@bht-berlin.de>
2022-01-14 17:11:20 +0100

    Multex 8.4 port to GitHub (#4)
    * Conflict with main resolved.

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #! /usr/bin/sh
-# deploy.sh                                                2007-09-19
-# 2011-03-30  Knabe  Derived from deploy.bat
-# Deploys the actual MulTEx release and the web site
+# deploy.sh                                                2022-01-07
+# Deploys the current MulTEx release and the web site
 echo $- $0
-set ue
-mvn -e clean compile
+set -ue
+#mvn -e clean compile
 # For some reason during the first compilation after clean we get an error:
 # multex.Exc.throwMe is not compilable (unreported exception Exc)
 # Thus once more:
-mvn deploy    org.codehaus.mojo:docbook-maven-plugin:transform  site:site  site:deploy
+sh mvnw clean deploy  site site:deploy
 
